83cf8c60a550a8fd6dd2491e277720dced89b0b7
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-15 10:54:37 +0200

    Use the heimdall methods for the http methods

--- src/commands/mxd-login.js
+++ src/commands/mxd-login.js
@@ -1,9 +1,8 @@
 module.exports = ({ heimdall, sessionStorage }) => async ({ args, loggedin, reply }) => {
   const account = await loggedin();
   const [userId, phrase] = args.split(' ');
-  const data = await heimdall.request('auth/login', {
-    body: { userId: userId, phrase: phrase, clientIp: '' },
-    method: 'post'
+  const data = await heimdall.post('auth/login', {
+    body: { userId: userId, phrase: phrase, clientIp: '' }
   });
   const session = {
     customer: { customerId: data.customer.customerId },
--- src/commands/mxd-logout.js
+++ src/commands/mxd-logout.js
@@ -1,8 +1,7 @@
 module.exports = ({ heimdall, sessionStorage }) => async ({ heimdallLoggedin, reply }) => {
   const { account, session } = await heimdallLoggedin();
-  await heimdall.request('auth/logout', {
-    headers: { 'mxd-session': session.sessionId },
-    method: 'post'
+  await heimdall.post('auth/logout', {
+    headers: { 'mxd-session': session.sessionId }
   });
   sessionStorage.del(account);
   reply.send('logout sucessful');
